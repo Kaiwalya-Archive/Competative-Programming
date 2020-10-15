ce4a6696b98f2e8ae96ed17ae993c073b20d3a05
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-10-15 11:14:27 +0530

    Reversing the array

--- Final500Sheet/ReversingTheArray/Solution.java
+++ Final500Sheet/ReversingTheArray/Solution.java
@@ -2,6 +2,33 @@ package Final500Sheet.ReversingTheArray;
 import java.util.*;
 public class Solution {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int array[] = new int[n];
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
 
+        int j = (array.length-1);
+        for (int i = 0; i < n; i++) {
+            if(j<=i){
+                break;
+            }
+            swap(i,j,array);
+            j--;
+        }
+        print(array);
+    }
+
+    public static void swap(int i, int j, int[] array){
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+
+    public static void print(int[] array){
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]+" ");
+        }
     }
 }
