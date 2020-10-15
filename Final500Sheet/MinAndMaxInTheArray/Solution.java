3faa889498955cfdb9d37a0829f7595876f17e28
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-10-15 11:33:07 +0530

    Minimum and Maximum from the array

---
+++ Final500Sheet/MinAndMaxInTheArray/Solution.java
@@ -0,0 +1,26 @@
+package Final500Sheet.MinAndMaxInTheArray;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int array[] = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
+        int min = array[0];
+        int max = array[0];
+
+        for (int i = 0; i < array.length; i++) {
+            if(array[i]<min){
+                min = array[i];
+            }else if(array[i]>max){
+                max = array[i];
+            }
+        }
+
+        System.out.println("Maximum is : "+max);
+        System.out.println("Minimum is : "+min);
+
+    }
+}
