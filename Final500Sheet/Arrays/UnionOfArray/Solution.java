2a821b6f6c105e937d8e71ad5bcd94e4efb0f655
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-10-17 18:23:56 +0530

    Program to find the union of the array

--- Final500Sheet/Arrays/UnionOfArray/Solution.java
+++ Final500Sheet/Arrays/UnionOfArray/Solution.java
@@ -9,11 +9,32 @@ public class Solution {
         for (int i = 0; i < set1.length; i++) {
             set1[i] = sc.nextInt();
         }
-        int set2[] = new int[n];
+        int set2[] = new int[m];
         for (int i = 0; i < set2.length; i++) {
             set2[i] = sc.nextInt();
         }
-
+        boolean repeated[] = new boolean[set2.length];
+        
+        int length = set1.length+ set2.length;
         //checking the union
+        for (int i = 0; i < set1.length; i++) {
+            for (int j = 0; j < set2.length; j++) {
+                if(set1[i]==set2[j]){
+                    length--;
+                    repeated[j]=true;
+                }
+            }
+        }
+
+        System.out.println("Length is : "+length);
+        for (int i = 0; i < set1.length; i++) {
+            System.out.print(set1[i]+" ");
+        }
+        for (int i = 0; i < set2.length; i++) {
+            if(repeated[i]==false){
+                System.out.print(set2[i]+" ");
+            }
+        }
     }
 }
+//test cases: 5 3 2 8 5 9 6 3 2 1
