1419d1102cfd38324734e63b5273c2daf9554671
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-10-17 17:47:05 +0530

    Doing union problem

---
+++ Final500Sheet/Arrays/UnionOfArray/Solution.java
@@ -0,0 +1,19 @@
+package Final500Sheet.Arrays.UnionOfArray;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+        int set1[] = new int[n];
+        for (int i = 0; i < set1.length; i++) {
+            set1[i] = sc.nextInt();
+        }
+        int set2[] = new int[n];
+        for (int i = 0; i < set2.length; i++) {
+            set2[i] = sc.nextInt();
+        }
+
+        //checking the union
+    }
+}
