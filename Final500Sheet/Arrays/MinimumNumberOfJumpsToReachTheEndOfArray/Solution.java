c2babbc2c7e1b71cc6cec45bb16c1f0d78ae52d6
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-10-18 17:41:36 +0530

    Trying to solve the minimum jump problem

---
+++ Final500Sheet/Arrays/MinimumNumberOfJumpsToReachTheEndOfArray/Solution.java
@@ -0,0 +1,19 @@
+package Final500Sheet.Arrays.MinimumNumberOfJumpsToReachTheEndOfArray;
+import java.util.*;
+public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        //Reading essential data from the user
+        int n = sc.nextInt();
+        int array[] = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
+        int minimumPath[] = new int[n];
+        int route[] = new int[n];
+
+        //Main Logic
+
+
+    }
+}
