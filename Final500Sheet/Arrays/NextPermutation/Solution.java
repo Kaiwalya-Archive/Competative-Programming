c096c7ed588ec25049493f8b913e68392760fb55
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2020-10-28 17:10:08 +0530

    Next Permutation solution is on the way

--- Final500Sheet/Arrays/NextPermutation/Solution.java
+++ Final500Sheet/Arrays/NextPermutation/Solution.java
@@ -1,4 +1,15 @@
 package Final500Sheet.Arrays.NextPermutation;
+import java.util.*;
 
 public class Solution {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n = sc.nextInt();
+        int array[] = new int[n];
+        for (int i = 0; i < array.length; i++) {
+            array[i] = sc.nextInt();
+        }
+
+
+    }
 }
