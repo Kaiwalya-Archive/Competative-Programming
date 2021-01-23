aa8ee209afe599f1861c1634b584df15af86152f
Kaiwalya Koparkar <kaiwalyakoparkar@gmail.com>
2021-01-23 09:50:43 +0530

    Quick Recall shifted to dsa preparation kit

--- QuickRecall/GoKoronaGo.java
+++
@@ -1,9 +0,0 @@
-//Go Korona Go
-
-package QuickRecall;
-
-public class GoKoronaGo{
-    public static void main(String args[]){
-        System.out.println("Go Korona Go");
-    }
-}
--- QuickRecall/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package QuickRecall;
-
-public class HelloWorld {
-    public static void main(String args[]){
-        System.out.println("Hello World, I am in java and trying output methods");
-    }
-}
